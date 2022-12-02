d97d150e6224d758a024b84b73336153294b6d87
Grzegorz Korzeniowski <grzegorz.korzeniowski@electrolux.com>
2022-12-02 14:09:24 +0100

    Initial commit

---
+++ constant.py
@@ -0,0 +1,6 @@
+WINS = [('A','Y'), ('B', 'Z'), ('C', 'X')]
+DRAWS = [('A','X'), ('B', 'Y'), ('C', 'Z')]
+POINTS = {'X': 1, 'Y': 2, 'Z': 3}
+NORMALIZE = {'A': 'X', 'B': 'Y', 'C': 'Z'}
+WINS2 = {'X': 'Z', 'Y': 'X', 'Z': 'Y'}
+RESULTS = {'X': 0, 'Y': 3, 'Z': 6}
---
+++ main.py
@@ -0,0 +1,78 @@
+import numpy
+from constant import *
+
+def get_calories():
+    calories = []
+    next = True
+    with open('input\day1.txt', 'r') as f:
+        while True:
+            l = f.readline()
+            if not l:
+                break
+            if (next):
+                calories.append(int(l))
+                next = False
+            else:
+                if (l != '\n'):
+                    calories[-1] = calories[-1] + int(l)
+                else:
+                    next = True
+    calories.sort(reverse=True)
+    return calories
+
+
+def day1_1():
+    calories = get_calories()
+    return calories[0]
+
+def day1_2():
+    calories = get_calories()
+    calories = numpy.sum(calories[0:3])
+    return calories
+
+
+def get_strategy():
+    strategy = []
+    with open('input\day2.txt', 'r') as f:
+        while True:
+            l = f.readline().strip()
+            if not l:
+                break
+            s = l.split(" ")
+            t = (s[0], s[1])
+            strategy.append(t)
+    return strategy
+
+
+def day2_1():
+    strategy = get_strategy()
+    result = 0
+    for s in strategy:
+        result += POINTS[s[1]]
+        if WINS.__contains__(s):
+            result += 6
+        elif DRAWS.__contains__(s):
+            result += 3
+    return result
+
+
+def day2_2():
+    strategy = get_strategy()
+    result = 0
+    for s in strategy:
+        expected = RESULTS[s[1]]
+        result += expected
+        them = NORMALIZE[s[0]]
+        if expected == 6:
+            i = list(WINS2.values()).index(them)
+            move = list(WINS2.keys())[i]
+        elif expected == 3:
+            move = them
+        else:
+            move = WINS2[them]
+        result += POINTS[move]
+    return result
+
+
+if __name__ == "__main__":
+    print(day2_2())
