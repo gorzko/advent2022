f703c05dc025a64181ded78f1e8d15a08a3d7051
gorzko <gkorzeniowski@outlook.com>
2023-02-26 21:01:41 +0100

    working on 11

--- day11.py
+++ day11.py
@@ -1,20 +1,20 @@
 from collections import deque
 
-monkeys = list()
-
 class Operation:
-
+    OPERATIONS = {
+        '+': lambda a, b: a + b,
+        '*': lambda a, b: a * b,
+    }
     def __init__(self, operation):
         self.__text = operation
         self.__num1, operation, self.__num2 = operation.split()
-        if operation == '*':
-            self.__operation = self._mul
-        else:
-            self.__operation = self._add
+        self.__operation = Operation.OPERATIONS[operation]
 
-    def _add(self, a, b):
+    @staticmethod
+    def _add(a, b):
         return a + b
-    def _mul(self, a, b):
+    @staticmethod
+    def _mul(a, b):
         return a * b
     def __eq__(self, other):
         return self.__text == str(other)
@@ -30,48 +30,22 @@ operation={repr(self.__text)})"
         b = old if self.__num2 == 'old' else int(self.__num2)
         return self.__operation(a, b)
 
-class Item:
-
-    def __init__(self, worry_level):
-        self.worry_level = int(worry_level)
-
-    def __eq__(self, other):
-        return self.worry_level == other.worry_level
-
-    def __int__(self):
-        return self.worry_level
-
-    def __add__(self, other):
-        if isinstance(other, Item):
-            other = other.worry_level
-        return Item(self.worry_level + other)
-
-    def __mul__(self, other):
-        if isinstance(other, Item):
-            other = other.worry_level
-        return Item(self.worry_level * other)
-
-    def __truediv__(self, other):
-        if isinstance(other, Item):
-            other = other.worry_level
-        return Item(self.worry_level / other)
-
-    def __str__(self):
-        return str(self.worry_level)
-
-    def __repr__(self):
-        return f'Item(worry_level={repr(self.worry_level)})'
 class Monkey:
+    monkeys = []
+    worry_level_divider = 0
+    @staticmethod
+    def __getitem__(item):
+        return Monkey.monkeys[item]
 
     def __init__(self, properties):
         properties[1] = properties[1][16:].replace(' ', '')
-        self.items = deque([Item(item) for item in properties[1].split(',')])
+        self.items = deque([int(item) for item in properties[1].split(',')])
         self.operation = Operation(properties[2][17:])
         self.action_test = int(properties[3][19:])
         self.action_true = int(properties[4][25:])
         self.action_false = int(properties[5][26:])
         self.inspections = 0
-        monkeys.append(self)
+        Monkey.monkeys.append(self)
         
     def __eq__(self, other):
         return self.items == other.items and self.operation == other.operation and \
@@ -91,39 +65,36 @@ class Monkey:
 test={repr(self.action_test)}, action_true={repr(self.action_true)} , action_false=\
 {repr(self.action_false)}, inspections={repr(self.inspections)})"
 
-    def play(self, worry_level_divider):
-        while len(self.items) > 0:
-            item = self.items[0]
-            other = self.test(item, worry_level_divider)
-            other = monkeys[other]
-            other.items.append(self.items.popleft())
-    def test(self, item, worry_level_divider):
+    def play(self):
+        while self.items:
+            other = self.test()
+            other = Monkey.monkeys[other]
+            other.items += [self.items.popleft()]
+
+    def test(self):
         self.inspections += 1
-        item.worry_level = self.operation.calc(item.worry_level)
-        if worry_level_divider > 0:
-            item.worry_level = item.worry_level // worry_level_divider
-        if item.worry_level % self.action_test == 0:
+        self.items[0] = self.operation.calc(self.items[0])
+        if Monkey.worry_level_divider > 0:
+            self.items[0] //= Monkey.worry_level_divider
+        if self.items[0] % self.action_test == 0:
             return self.action_true
         else:
             return self.action_false
 
+
 def read_file(file: str):
     with open('input\\' + file, 'r') as f:
-        properties = f.readlines()
-    properties = [line.strip() for line in properties if line.strip() != '']
-    monkeys_count = int(len(properties) / 6)
-    for i in range(0, monkeys_count):
-        start = i * 6
-        end = start + 6
-        Monkey(properties[start:end])
+        properties = [line.strip() for line in f.readlines() if line.strip() != '']
+    [Monkey(properties[i:i + 6]) for i in range(0, len(properties), 6)]
 
 def day11(file: str, rounds: int, worry_level_divider: int):
     read_file(file)
-    for i in range(0, rounds):
-        for monkey in monkeys:
-            monkey.play(worry_level_divider)
-    monkeys.sort(reverse=True)
-    return monkeys[0].inspections * monkeys[1].inspections
+    Monkey.worry_level_divider = worry_level_divider
+    for i in range(rounds):
+        for monkey in Monkey.monkeys:
+            monkey.play()
+    Monkey.monkeys.sort(reverse=True)
+    return Monkey.monkeys[0].inspections * Monkey.monkeys[1].inspections
 
 if __name__ == '__main__':
     print(day11('day11t.txt', 10000, 0))
