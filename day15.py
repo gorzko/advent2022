3e3036a31a363ba8adea97a92997201e619adb91
gorzko <gkorzeniowski@outlook.com>
2023-03-17 18:57:18 +0100

    day 15_2 tests work (2nd attempt)

--- day15.py
+++ day15.py
@@ -9,6 +9,9 @@ def read_file(file):
         sensors.append((sensor[0], sensor[1], beacon[0], beacon[1], distance))
 
     return sensors
+
+def in_area(sensor, x, y):
+    return (deviation := sensor[4] - abs(y - sensor[1])) >= 0 and sensor[0] - deviation <= x <= sensor[0] + deviation
 def day15_1(file, y):
     sensors = read_file(file)
     ranges = [range(s[0] - deviation, s[0] + deviation + 1) for s in sensors if \
@@ -19,20 +22,23 @@ def day15_1(file, y):
     beacons = set([b[2] for b in sensors if b[3] == y])
     return len(unique - beacons)
 
-def day15_2(file, max):
+def day15_2(file, ceil):
     sensors = read_file(file)
-    max += 1
-    for i in range(max):
-        ranges = [range(s[0] - deviation, s[0] + deviation + 1) for s in sensors if \
-                  (deviation := s[4] - abs(i - s[1])) >= 0]
-        unique = set()
-        for r in ranges:
-            unique.update(r)
-        unique = [r for r in unique if 0 <= r < max]
-        if len(unique) < max:
-            return [x for x in range(max) if x not in unique][0] * 4000000 + i
-
-    return sensors
+    unique = set()
+    for s in sensors:
+        outline = set([((max(0, s[0] - deviation - 1), i), (min(ceil, s[0] + deviation + 1), i)) for i in
+                       range(max(0, s[1] - s[4] - 1), min(ceil, s[1] + s[4] + 2)) if
+                       (deviation := s[4] - abs(i - s[1])) >= 0])
+        outline = [p for p in outline for p in p]
+        temp_outline = outline.copy()
+        for o in outline:
+            for ss in sensors:
+                if in_area(ss, o[0], o[1]):
+                    temp_outline.remove(o)
+                    break
+        unique.update(temp_outline)
+    unique = list(unique)[0]
+    return unique[0] * 4000000 + unique[1]
 
 
 if __name__ == '__main__':
