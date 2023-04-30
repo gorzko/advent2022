99f4f4ca1e8dd0b1d3e667df2a330507285e852a
gorzko <gkorzeniowski@outlook.com>
2023-04-30 19:41:51 +0200

    day17 jet pushes

--- day17.py
+++ day17.py
@@ -1,54 +1,53 @@
-# ROCKS = [
-#     [
-#         [(3, 2), (3, 5)],
-#     ],
-#     [
-#         (3, 3),
-#         [(4, 2), (4, 4)],
-#         (5, 3),
-#     ],
-#     [
-#         [(3, 2), (3, 4)],
-#         (4, 4),
-#         (5, 4),
-#     ],
-#     [
-#         (3, 2),
-#         (4, 2),
-#         (5, 2),
-#         (6, 2),
-#     ],
-#     [
-#         [(3, 2), (3, 3)],
-#         [(4, 2), (4, 3)],
-#     ],
-# ]
-
 from collections import deque
 
 CHAMBER = {i : 0 for i in range(7)}
 
-ROCKS = [
-    {0: (0, 0), 1: (0, 0), 2: (1, 3), 3: (1, 3), 4: (1, 3), 5: (1, 3), 6: (0, 0)},
-    {0: (0, 0), 1: (0, 0), 2: (1, 4), 3: (3, 3), 4: (1, 4), 5: (0, 0), 6: (0, 0)},
-    {0: (0, 0), 1: (0, 0), 2: (1, 3), 3: (1, 3), 4: (3, 3), 5: (0, 0), 6: (0, 0)},
-    {0: (0, 0), 1: (0, 0), 2: (4, 3), 3: (0, 0), 4: (0, 0), 5: (0, 0), 6: (0, 0)},
-    {0: (0, 0), 1: (0, 0), 2: (2, 3), 3: (2, 3), 4: (0, 0), 5: (0, 0), 6: (0, 0)},
-]
+ROCKS = deque([
+    deque([(0, 0), (0, 0), (1, 3), (1, 3), (1, 3), (1, 3), (0, 0)]),
+    deque([(0, 0), (0, 0), (1, 4), (3, 3), (1, 4), (0, 0), (0, 0)]),
+    deque([(0, 0), (0, 0), (1, 3), (1, 3), (3, 3), (0, 0), (0, 0)]),
+    deque([(0, 0), (0, 0), (4, 3), (0, 0), (0, 0), (0, 0), (0, 0)]),
+    deque([(0, 0), (0, 0), (2, 3), (2, 3), (0, 0), (0, 0), (0, 0)]),
+])
+
+ROCKS_COUNTER = 0
+
+JETS = deque()
 
 def read_file(file):
+    global JETS
     with open('input\\' + file, 'r') as f:
-        jets = deque([1 if j == '>' else -1 for j in f.read()])
-    return jets
+        JETS = deque([1 if j == '>' else -1 for j in f.read()])
+
+def get_rock():
+    global ROCKS, ROCKS_COUNTER
+    rock = ROCKS[0].copy()
+    ROCKS.append(ROCKS.popleft())
+    ROCKS_COUNTER += 1
+    # set rock values here
+    return rock
+
+def push_rock(rock):
+    global JETS
+    direction = JETS[0]
+    JETS.append(JETS.popleft())
+
+    if direction == -1:
+        if rock[0][0] == 0:
+            rock.append(rock.popleft())
+    else:
+        if rock[-1][0] == 0:
+            rock.appendleft(rock.pop())
 
 def day17_1(file):
-    jets = read_file(file)
+    global CHAMBER, ROCKS, ROCKS_COUNTER
+    read_file(file)
+    rock = None
 
-    rocks_counter = 0
-
-    for j in jets:
-        if rocks_counter == 2022:
-            break
+    while ROCKS_COUNTER < 2022:
+        if not rock:
+            rock = get_rock()
+        push_rock(rock)
 
 
 def day17_2(file):
