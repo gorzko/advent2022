d6f70044c0178deee31f92318c6c17780126aeb4
gorzko <gkorzeniowski@outlook.com>
2023-06-10 20:28:10 +0200

    day18 day 1 works

--- day18.py
+++ day18.py
@@ -6,13 +6,14 @@ def read_file(file):
         return frozenset(tuple(int(d) for d in l.split(',')) for l in f.readlines())
 
 
-def is_offset_one(a):
-    return a[1] - a[0] == 1
+@cache
+def is_offset_one(a, b):
+    return abs(a - b) == 1
 
 @cache
 def is_adjacent(p):
     return len([i for i in map(operator.eq, p[0], p[1]) if i]) == 2 \
-and len([i for i in map(lambda a, b: abs(a - b) == 1, p[0], p[1]) if i]) == 1
+and len([i for i in map(is_offset_one, p[0], p[1]) if i]) == 1
 
 
 def count_adjacent(p, cubes):
